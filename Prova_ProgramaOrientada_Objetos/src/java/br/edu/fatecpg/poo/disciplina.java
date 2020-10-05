23b0331cf3cfa4f7e19aa654fb18648736a8c16f
Uilton Santos <uilton.amaral@fatec.sp.gov.br>
2020-10-05 13:00:49 -0300

    5- Criação da Classe disciplina
    Criação da Classe Disciplina que retorna os valores e o arraylist

---
+++ Prova_ProgramaOrientada_Objetos/src/java/br/edu/fatecpg/poo/disciplina.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fatecpg.poo;
+import java.util.ArrayList;
+/**
+ *
+ * @author uilsa
+ */
+public class disciplina {
+    private String nome, ementa;
+    private int semestre;
+    private ArrayList<Double> nota = new ArrayList();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmenta() {
+        return ementa;
+    }
+
+    public void setEmenta(String ementa) {
+        this.ementa = ementa;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+
+    public ArrayList<Double> getNota() {
+        return nota;
+    }
+
+    public void setNota(Double dnota, int i) {
+        this.nota.add(i, dnota);
+    }
+    
+    public disciplina(String nome, String ementa, int semestre){
+        this.nome = nome;
+        this.ementa = ementa;
+        this.semestre = semestre;
+    }
+    
+    public ArrayList GetMaterias(int semestre){
+        ArrayList materias = new ArrayList();
+        materias.clear();
+        switch (semestre){
+            case 1: 
+                materias.add("Semestre 1 - Materia 1");
+                materias.add("Semestre 1 - Materia 2");
+                materias.add("Semestre 1 - Materia 3");
+                materias.add("Semestre 1 - Materia 4");
+                materias.add("Semestre 1 - Materia 5");
+                break;
+            case 2: 
+                materias.add("Semestre 2 - Materia 1");
+                materias.add("Semestre 2 - Materia 2");
+                materias.add("Semestre 2 - Materia 3");
+                materias.add("Semestre 2 - Materia 4");
+                materias.add("Semestre 2 - Materia 5");
+                break;
+            case 3: 
+                materias.add("Semestre 3 - Materia 1");
+                materias.add("Semestre 3 - Materia 2");
+                materias.add("Semestre 3 - Materia 3");
+                materias.add("Semestre 3 - Materia 4");
+                materias.add("Semestre 3 - Materia 5");
+                break;
+            case 4: 
+                materias.add("Semestre 4 - Materia 1");
+                materias.add("Semestre 4 - Materia 2");
+                materias.add("Semestre 4 - Materia 3");
+                materias.add("Semestre 4 - Materia 4");
+                materias.add("Semestre 4 - Materia 5");
+                break;
+            case 5:                     
+                materias.add("Programação Orientada a Objetos");
+                materias.add("Engenharia de Software III");
+                materias.add("Laboratorio de Banco de Dados");
+                materias.add("Gestão de Projetos");
+                materias.add("Metodologia de Pesquisa");
+                materias.add("Inglês IV");
+                break;
+            case 6: 
+                materias.add("Semestre 6 - Materia 1");
+                materias.add("Semestre 6 - Materia 2");
+                materias.add("Semestre 6 - Materia 3");
+                materias.add("Semestre 6 - Materia 4");
+                materias.add("Semestre 6 - Materia 5");  
+                break;
+        }
+        return materias;
+    }
+}
